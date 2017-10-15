1177f120194f5d4299198c652629cfcbb5739994
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-15 12:24:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-
+    binding.pry
     song = self.new(file_name.split(" - ")[1])
     #binding.pry
     song.artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
